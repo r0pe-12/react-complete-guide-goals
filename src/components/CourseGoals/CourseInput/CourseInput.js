bb5d2df9d0d90b39de40e6f1189738d41fa6a4bc
Petar Simonovic <simonovicp12@gmail.com>
2023-04-17 22:42:06 +0200

    dynamic css modules

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,42 +1,42 @@
 import React, {useState} from 'react';
 
 import Button from '../../UI/Button/Button';
-import './CourseInput.css';
+import styles from './CourseInput.module.css';
 import styled from "styled-components";
 
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-  }
-
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid #ccc;
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-  
-  &.invalid label {
-    color: red;
-  }
-
-  &.invalid input {
-    border-color: red;
-    background: salmon;
-  }
-`;
+// const FormControl = styled.div`
+//   margin: 0.5rem 0;
+//
+//   & label {
+//     font-weight: bold;
+//     display: block;
+//     margin-bottom: 0.5rem;
+//   }
+//
+//   & input {
+//     display: block;
+//     width: 100%;
+//     border: 1px solid #ccc;
+//     font: inherit;
+//     line-height: 1.5rem;
+//     padding: 0 0.25rem;
+//   }
+//
+//   & input:focus {
+//     outline: none;
+//     background: #fad0ec;
+//     border-color: #8b005d;
+//   }
+//
+//   &.invalid label {
+//     color: red;
+//   }
+//
+//   &.invalid input {
+//     border-color: red;
+//     background: salmon;
+//   }
+// `;
 
 const CourseInput = props => {
     const [enteredValue, setEnteredValue] = useState('');
@@ -60,12 +60,12 @@ const CourseInput = props => {
 
     return (
         <form onSubmit={formSubmitHandler}>
-            <FormControl className={!isValid && 'invalid'} invalid={!isValid}>
+            <div className={`${styles['form-control']} ${!isValid && styles.invalid}`}>
                 {/*<div className={`form-control ${!isValid ? 'invalid' : ''}`}>*/}
                 <label>Course Goal</label>
                 <input type="text" onChange={goalInputChangeHandler}/>
                 {/*</div>*/}
-            </FormControl>
+            </div>
             <Button type="submit">Add Goal</Button>
         </form>
     );
